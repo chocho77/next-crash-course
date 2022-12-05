7484afc4c77f36e0290888f11de63876b1acaebc
User <user@Users-MacBook.local>
2022-12-05 11:26:28 +0200

    Committer: Chavdar <chavdar@Users-MacBook.local>  On branch main  Your branch is up-to-date with 'origin/main'.  Changes to be committed: 	modified:   components/Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,7 @@ const Layout = ({children}) => {
     return (
         <div className={styles.container}>
             <main className={styles.main}>
+                <h1>Hello</h1>
                 {children}
             </main>
         </div>
